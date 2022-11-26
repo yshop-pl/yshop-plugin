e1f410f7a05489d36713bf554845d710f1b40d39
Wiktor Wilk <mcserw4@wp.pl>
2022-11-26 21:03:47 +0100

    init bungee module

---
+++ bungee/src/main/java/pl/yshop/plugin/bungee/BungeePlugin.java
@@ -0,0 +1,16 @@
+package pl.yshop.plugin.bungee;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class BungeePlugin extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
